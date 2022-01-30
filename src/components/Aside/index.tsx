537846f258c16fdf8e0effa503a9c0498763e24c
brunnacroches <brunnacroches@gmail.com>
2022-01-30 20:44:02 -0300

    commit on "main"

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,38 @@
 import React from "react";
-import { Container } from "./styles";
+
+import LogoIzaI from '../../assets/svg/logo-i.svg';
+import LogoIzaPonto from '../../assets/svg/logo-ponto.svg';
+import LogoIzaZa from '../../assets/svg/logo-za.svg';
+import Seguidores from '../../assets/svg/peple.svg';
+import Painel from '../../assets/svg/grid.svg';
+import Pagamentos from '../../assets/svg/card.svg';
+import DadosCadastrais from '../../assets/svg/logo-za.svg';
+import Suporte from "../../pages/Suporte";
+import Blogging from "../../assets/svg/blogging.svg";
+
+
+import {
+  Container,
+  Header,
+  LogImg,
+  MenuContainer,
+  MenuItemLink
+} from './styles';
+
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogImg className="LogoIza" src={LogoIzaI} alt="Logo Minha Carteira" />
+        <LogImg  className="LogoIzaPonto" src={LogoIzaPonto} alt="Logo Minha Carteira" />
+        <LogImg  className="LogoIzaZa" src={LogoIzaZa} alt="Logo Minha Carteira" />
+      </Header>
+      <MenuContainer>
+        <MenuItemLink href="#">
+          Dashboard
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 };
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,22 +1,29 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+/* ÁREA QUE QUE O MENU VAI FICAR DISPONÍVEL */
   grid-area: AS;
   color: ${(props) => props.theme.colors.info};
   background-color: ${(props) => props.theme.colors.primary};
+  /* PARA QUE TODO O CONTEÚDO ANDE PARTINDO DA ESQUERDA */
+  padding-left: 20px;
 `;
 
-/**
- */
-/**
- *  * IMPORTANTE
- *  ! ATENÇÃO!
- *  ? DUVIDA
- *  TODO MAIUSCULO
- *  @param
- * @param myParam The parameter of this method
- *
- */
-/*
- * ? ELE TEM QUE FICAR POSICIONADO NO MEU AS >
- * ? ELE COCUPA A LINHA INTEIRA (PRIMEIRA E SEGUNDA LINHA DA PRIMEIRA COLUNA ⤵︎  */
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+`;
+
+export const LogImg = styled.img`
+  height: 56px;
+  width: 100%;
+`;
+export const MenuContainer = styled.nav`
+`;
+
+export const MenuItemLink = styled.a`
+`;
+
+
+
+
