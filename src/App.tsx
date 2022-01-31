cbd2f497291eb1d5e475fea7b0f94a132fb89612
brunnacroches <brunnacroches@gmail.com>
2022-01-31 19:37:11 -0300

    commit on "main"

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
+
 import UncontrolledLottie from './components/UncontrolledLottie';
 import ControlledLottie from './components/ControlledLottie';
 import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
+// TEMA 
 import light from './styles/themes/light';
+
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={light}>
+    <ThemeProvider theme ={ light }>
       <GlobalStyles />
       <Layout>
       <div className="lotties">
           <UncontrolledLottie />
           <ControlledLottie />
       </div>
+      <Dashboard/>
       </Layout>
     </ThemeProvider>
   );
