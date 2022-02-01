c0aba6190888dcfa96eed458b1323292c5ff55c1
brunnacroches <brunnacroches@gmail.com>
2022-02-01 19:51:47 -0300

    commin on "main|

--- src/components/DataTables/styles.ts
+++ src/components/DataTables/styles.ts
@@ -1,13 +1,28 @@
 import styled from 'styled-components';
-import { Controllers } from '../ContentHeader/styles';
+// import { Controllers } from '../ContentHeader/styles';
 
 
 export const Container = styled.div`
-
+  border-radius:16px 16px 16px 16px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
   background-color: ${props => props.color};
 
-  list 
+  list-style: none;
+  margin: 10px 0;
+  padding: 12px 12px;
 
+  display: flex;
+  /* COLOCAR UM PARA CADA LADO */
+  justify-content: space-between;
+  align-items: center;
+
+  cursor: pointer;
+  transition: all .3s;
+
+  &:hover {
+    opacity: 0.7;
+    transform: translateX(10px);
+  }
 `;
 
 export const Tag = styled.div`
