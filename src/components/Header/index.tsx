5779011e6e82c66d002b6284e9a1c9eb740d4a05
brunnacroches <brunnacroches@gmail.com>
2022-02-08 00:44:01 -0300

    commit on "main"

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; 
+import LogoIzaI from '../../assets/svg/logo-iza-i-orange.svg';
+import LogoIzaPonto from '../../assets/svg/logo-iza-ponto-orange.svg';
+import LogoIzaZa from '../../assets/svg/logo-iza-za-orange.svg';
+import { 
+  Container, 
+  Nav, 
+  Navbar 
+} from 'react-bootstrap';
+
+
+import {
+  // HeaderLogo,
+  LogoIzaP,
+  LogoIzaIP,
+  LogoIza
+} from './styles';
 
 const Header: React.FC = () => {
   return (
     <>
   <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="#home"> Seguros Iza </Navbar.Brand>
+    <Navbar.Brand href="#home"> 
+        {/* <HeaderLogo> */}
+          <LogoIzaP src={LogoIzaPonto} alt=""/>
+          <LogoIzaIP src={LogoIzaI} alt="" />
+          <LogoIza src={LogoIzaZa} alt="" />
+        {/* </HeaderLogo> */}
+    </Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link as={Link} className="nav-link" to="/">Início</Nav.Link>
-      <Nav.Link as={Link} className="nav-link" to="/tarefas">Tarefas</Nav.Link>
+      <Nav.Link as={Link} className="nav-link" to="/tarefas">Crud</Nav.Link>
+      <Nav.Link as={Link} className="nav-link" to="/">Termos</Nav.Link>
+      <Nav.Link as={Link} className="nav-link" to="/">Login</Nav.Link>
+      <Nav.Link as={Link} className="nav-link" to="/">Fazer cotação</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+
+// !HEADER => 
+export const HeaderLogo = styled.img`
+height: 48px;
+  width: 20px;
+  position: relative;
+  left: -10px;
+  top: 30px;
+
+  cursor: pointer;
+  transition: all .3s;
+
+  &:hover {
+    opacity: 0.7;
+    transform: translateX(10px);
+  }
+  
+`;
+// ! LOGO IZA => I PONTO
+export const LogoIzaP = styled.img`
+  height: 18px;
+  width: 15px;
+  position: relative;
+  left: 68px;
+  top: -30px;
+
+  cursor: pointer;
+  transition: all .3s;
+
+  &:hover {
+    opacity: 0.7;
+    transform: translateX(10px);
+  }
+
+`;
+// ! LOGO IZA => IZA
+export const LogoIzaIP = styled.img`
+  height: 50px;
+  width: 60px;
+  position: relative;
+  top: 10px;
+  left: 30px;
+  cursor: pointer;
+  transition: all .3s;
+
+  &:hover {
+    opacity: 0.7;
+    transform: translateX(10px);
+  }
+
+`;
+
+// ! LOGO IZA => IZA
+export const LogoIza = styled.img`
+  height: 100px;
+  width: 105px;
+  position: relative;
+  top: 10px;
+  left: 15px;
+  cursor: pointer;
+  transition: all .3s;
+
+  &:hover {
+    opacity: 0.7;
+    transform: translateX(10px);
+  }
+
+`;
